74ed56db953d6dbcb15e88211f03cd81bc023d94
dhansenaz <dhansen@allthingsdot.com>
2019-01-16 10:51:45 -0800

    added javascript to hide buttons on click

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
+// MODAL OPEN AND CLOSE
+
 let modal = document.getElementById('forestModal');
 let btn = document.getElementById('forestButton');
 let span = document.getElementsByClassName('close')[0];
+let btnHide = document.getElementById('buttons')
 
 btn.onclick = function () {
     console.log('button clicked')
     modal.style.display = 'block'
+}
+
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(e) {
+    if(e.target === modal) {
+        modal.style.display = 'none'
+    }
+}
+
+//BUTTON HIDE
+
+
+btnHide.onclick = function () {
+    console.log('button clicked')
+    document.getElementById('buttons').style.visibility='hidden';
 }
 
 span.onclick = function() {
